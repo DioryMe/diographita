25c528e2fa9aa31e68e18afb5d69125e2130f090
Jouni Alanen <jouni@diory.me>
2025-07-10 20:45:43 +0300

    Monorepo base, tsconfig, vite seems to work
    - upgrade Yarn to v4

--- packages/desktop-client/src/main.tsx
+++ packages/desktop-client/src/main.tsx
@@ -1,8 +1,9 @@
+import { helloWorld } from "@diographita/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div>Hello world!</div>
+    <div>{helloWorld()}</div>
   </React.StrictMode>
 );
