484a4ff86c079c4be62ef37549c69aa2fd6ef4ca
Jouni Alanen <jouni@diory.me>
2025-07-11 07:36:06 +0300

    Attempt to get workspace linked modules to work with preload.js

--- packages/desktop-electron/src/preload.ts
+++ packages/desktop-electron/src/preload.ts
@@ -1,9 +1,9 @@
 import { contextBridge, ipcRenderer } from "electron";
-// import { IPC_ACTIONS } from "@diographita/core";
+import { IPC_ACTIONS } from "@diographita/core";
 
 contextBridge.exposeInMainWorld("electronAPI", {
-  helloWorld: () => ipcRenderer.invoke("HELLO_WORLD"),
-  selectFolder: () => ipcRenderer.invoke("SELECT_FOLDER"),
-  // helloWorld: () => ipcRenderer.invoke(IPC_ACTIONS.HELLO_WORLD),
-  // selectFolder: () => ipcRenderer.invoke(IPC_ACTIONS.SELECT_FOLDER),
+  // helloWorld: () => ipcRenderer.invoke("HELLO_WORLD"),
+  // selectFolder: () => ipcRenderer.invoke("SELECT_FOLDER"),
+  helloWorld: () => ipcRenderer.invoke(IPC_ACTIONS.HELLO_WORLD),
+  selectFolder: () => ipcRenderer.invoke(IPC_ACTIONS.SELECT_FOLDER),
 });
